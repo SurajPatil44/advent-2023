c680b25dfdb1386e35754b84c889229fecb2a4bc
SurajPatil44 <suraj.patil716@gmail.com>
2023-12-13 22:54:04 +0530

    day 1 part 1

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,3 +1,37 @@
+///https://adventofcode.com/2023/day/1
+use std::env;
+use utils::Content;
+
+fn take_while<'a, I>(it: &mut I, P: impl Fn(char) -> bool) -> (usize, usize)
+where
+    I: Iterator<Item = char>,
+{
+    let mut i = 0;
+    loop {
+        let c = it.next().unwrap();
+        if P(c) {
+            break;
+        }
+        i += 1;
+    }
+    (i, i + 1)
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut args = std::env::args();
+    let _pgm_name = args.next();
+    let fname = args.next().unwrap_or("sample.txt".to_string());
+    let mut sum = 0;
+    let mut reader = Content::read_from_file(&fname);
+    for l in reader {
+        let sz = l.len();
+        let mut it = l.chars();
+        let mut itr = l.chars().rev();
+        let (n1, _) = take_while(&mut it, |c| c >= '0' && c <= '9');
+        let (n2, _) = take_while(&mut itr, |c| c >= '0' && c <= '9');
+        let (first, second) = (l.as_bytes()[n1], l.as_bytes()[sz - 1 - n2]);
+        let num = (first as u8 - b'0') as usize * 10 + (second as u8 - b'0') as usize;
+        sum += num;
+    }
+    dbg!(sum);
 }

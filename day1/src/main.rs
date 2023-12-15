f8d2908733f535df977ce63bb278ed5279158b3f
Suraj Patil <sdpatil@qti.qualcomm.com>
2023-12-15 14:11:52 +0530

    &str instead of iterator

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,20 +1,20 @@
 ///https://adventofcode.com/2023/day/1
-use std::env;
 use utils::Content;
 
-fn take_while<'a, I>(it: &mut I, P: impl Fn(char) -> bool) -> (usize, usize)
-where
-    I: Iterator<Item = char>,
-{
-    let mut i = 0;
-    loop {
-        let c = it.next().unwrap();
-        if P(c) {
-            break;
-        }
-        i += 1;
-    }
-    (i, i + 1)
+
+fn take_while2(it : &str,p: impl Fn(char) -> bool) -> (&str,&str) {
+	let mut i = 0;
+	loop {
+		if i >= it.len(){
+			break;
+		}
+		let c = it.as_bytes()[i] as char;
+		if p(c) {
+			break;
+		}
+		i+=1
+	}
+	(&it[..i],&it[i..])
 }
 
 fn main() {
@@ -22,15 +22,13 @@ fn main() {
     let _pgm_name = args.next();
     let fname = args.next().unwrap_or("sample.txt".to_string());
     let mut sum = 0;
+	#[allow(unused_mut)]
     let mut reader = Content::read_from_file(&fname);
     for l in reader {
-        let sz = l.len();
-        let mut it = l.chars();
-        let mut itr = l.chars().rev();
-        let (n1, _) = take_while(&mut it, |c| c >= '0' && c <= '9');
-        let (n2, _) = take_while(&mut itr, |c| c >= '0' && c <= '9');
-        let (first, second) = (l.as_bytes()[n1], l.as_bytes()[sz - 1 - n2]);
-        let num = (first as u8 - b'0') as usize * 10 + (second as u8 - b'0') as usize;
+        let itr : String = l.chars().rev().collect();
+		let (ss1,ss2) = take_while2(&l,|c| c >= '0' && c <= '9');
+		let (ssr1,ssr2) = take_while2(&itr,|c| c >= '0' && c <= '9');
+		let num = (ss2.as_bytes()[0] as u8 - b'0') as usize * 10 + (ssr2.as_bytes()[0] as u8 - b'0') as usize;
         sum += num;
     }
     dbg!(sum);

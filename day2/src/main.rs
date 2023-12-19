0d891a3dba0751660234ec814c4c0e394a021815
SurajPatil44 <suraj.patil716@gmail.com>
2023-12-19 23:37:58 +0530

    day2 part 2

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -58,10 +58,12 @@ fn main() {
         green: 13,
     };
     let mut ids = Vec::new();
+    let mut max_runs: Vec<(u32, u32, u32)> = Vec::new();
     #[allow(unused_mut)]
     let mut reader = Content::read_from_file(&fname);
     for line in reader {
         let mut found = false;
+        let (mut rd, mut gr, mut bl) = (0, 0, 0);
         let (game_id, line) = partition(&line, ':');
         let (_, game_id) = partition(&game_id, ' ');
         let game_id: u32 = game_id.parse().unwrap();
@@ -69,13 +71,27 @@ fn main() {
             let g = Game::from_str(&part);
             if !check(&g, &other) {
                 found = true;
-                break;
+            }
+            if g.red > rd {
+                rd = g.red;
+            }
+            if g.blue > bl {
+                bl = g.blue;
+            }
+            if g.green > gr {
+                gr = g.green;
             }
         }
         if !found {
             ids.push(game_id);
         }
+        max_runs.push((rd, gr, bl));
     }
     let sum: u32 = ids.iter().sum();
     dbg!(sum);
+    let mut pow = 0;
+    for (r, g, b) in &max_runs {
+        pow += r * g * b;
+    }
+    dbg!(pow);
 }
